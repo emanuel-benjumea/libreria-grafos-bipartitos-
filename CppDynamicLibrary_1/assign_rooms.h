b1b886deb16e7a64dadd44ace1f35992885a6e20
Emanuel Benjumea <emanuelema2013rapido@gmail.com>
2023-11-23 22:21:29 -0500

    grafos
    se agregan archivos para la elaboracion de libreria con grafos

---
+++ CppDynamicLibrary_1/assign_rooms.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   assign_rooms.h
+ * Author: Emanuel
+ *
+ * Created on 23 de noviembre de 2023, 10:19 p. m.
+ */
+
+#ifndef ASSIGN_ROOMS_H
+#define ASSIGN_ROOMS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSIGN_ROOMS_H */
+
---
+++ CppDynamicLibrary_1/main.c
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: Emanuel
+ *
+ * Created on 23 de noviembre de 2023, 10:20 p. m.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
